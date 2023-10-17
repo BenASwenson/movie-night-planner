c86aca4838b0a78e040f858f6eacb8d6bdac15b0
Elza Contiero <contieroelza@gmail.com>
2023-10-17 12:44:47 +0100

    Using new modern API WebClient

--- movie-planner/src/test/java/com/sparta/movieplanner/tmdb/TmdbApiTest.java
+++ movie-planner/src/test/java/com/sparta/movieplanner/tmdb/TmdbApiTest.java
@@ -5,21 +5,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import com.sparta.movieplanner.tmdb.Genre;
 
+// @SpringBootTest
 public class TmdbApiTest {
+
+    static final String key = "2e778d5f31e240b493d778371b6bbcb8";
+
     @Test
-    void genreTest() { 
-        String url = "https://api.themoviedb.org/3/genre/movie/list?api_key=2e778d5f31e240b493d778371b6bbcb8";
-        RestTemplate restTemplate = new RestTemplate();
-        
-        GenreResponse response = restTemplate.getForObject(url, GenreResponse.class);
-        for(Genre g: response.genres){
+    void genreTest() {
+        // TODO: Refactor the below into a Tmdb java api class.
+        WebClient client = WebClient.create("https://api.themoviedb.org");
+        Mono<GenreResponse> response = client.get()
+                .uri("/3/genre/movie/list?api_key={key}", key)
+                .retrieve()
+                .bodyToMono(GenreResponse.class);
+
+        GenreResponse genreResponse = response.block();
+        for(Genre g: genreResponse.genres){
             System.out.println("Id = " + g.id + " name = " + g.name);
         }
-        
-
     }
-    
+
 }
