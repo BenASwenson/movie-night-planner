23a948dc0e2372db6556f54a4ff05ef687442301
Jade Sale <jadesale1991@googlemail.com>
2023-10-18 19:39:42 +0100

    hidden watchlist and calendar if user is not logged in. These will only be able to be accessed if the user is logged in

--- movie-planner/src/main/java/com/sparta/movieplanner/controllers/web/HomepageHandler.java
+++ movie-planner/src/main/java/com/sparta/movieplanner/controllers/web/HomepageHandler.java
@@ -1,7 +1,9 @@
 package com.sparta.movieplanner.controllers.web;
 
+import com.sparta.movieplanner.entities.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,13 +17,21 @@ public class HomepageHandler {
     Logger log = LoggerFactory.getLogger(HomepageHandler.class);
 
     @GetMapping("homepage")
-    public String homepage(@RequestParam(name = "logout", required = false) String logout, Model model) {
+    public String homepage(@RequestParam(name = "logout", required = false) String logout, Model model, Authentication authentication) {
         String homepage = "movie/searchMovie";
         log.info("loading homepage: " + homepage + ".html");
 
         if (logout != null) {
             log.info("Logout was successful");
             model.addAttribute("logoutSuccess", true);
+        }
+        
+        if (authentication != null) {
+            log.info("user is authenticated");
+            model.addAttribute("authenticated", true);
+        } else {
+            log.info("user is not authenticated or not logged in");
+            model.addAttribute("authenticated", false);
         }
 
         return homepage;
