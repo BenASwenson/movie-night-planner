208227d9bd7d635e0ceeb238d20cfd10352e194a
Elza Contiero <contieroelza@gmail.com>
2023-10-19 11:31:27 +0100

    Add skeleton of MovieService and MovieDTO

--- movie-planner/src/main/java/com/sparta/movieplanner/controllers/web/PageHandler.java
+++ movie-planner/src/main/java/com/sparta/movieplanner/controllers/web/PageHandler.java
@@ -1,15 +1,21 @@
 package com.sparta.movieplanner.controllers.web;
 
+import com.sparta.movieplanner.service.MovieService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class PageHandler {
     Logger log = LoggerFactory.getLogger(PageHandler.class);
+
+    @Autowired
+    MovieService movieService;
 
     @GetMapping("login")
     public String loginPage() {
@@ -26,6 +32,8 @@ public class PageHandler {
 
         return movies;
     }
+
+    // TODO: add or change the above to receive the request for a movie given a query
 
     @GetMapping("register")
     public String signUp() {
