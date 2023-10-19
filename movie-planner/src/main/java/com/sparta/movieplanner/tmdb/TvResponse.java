4c0e61d1bced7e9c96a22fe120ba18faf5896d9e
Elza Contiero <contieroelza@gmail.com>
2023-10-19 17:25:55 +0100

    Add TV entities and findTV by title and add test for the code

---
+++ movie-planner/src/main/java/com/sparta/movieplanner/tmdb/TvResponse.java
@@ -0,0 +1,52 @@
+package com.sparta.movieplanner.tmdb;
+
+import java.util.ArrayList;
+
+public class TvResponse {
+    private int page;
+    private ArrayList<TvShort> results;
+    private int total_pages;
+    private int total_results;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public ArrayList<TvShort> getResults() {
+        return results;
+    }
+
+    public void setResults(ArrayList<TvShort> results) {
+        this.results = results;
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(int total_pages) {
+        this.total_pages = total_pages;
+    }
+
+    public int getTotal_results() {
+        return total_results;
+    }
+
+    public void setTotal_results(int total_results) {
+        this.total_results = total_results;
+    }
+
+    @Override
+    public String toString() {
+        return "TvResponse{" +
+                "page=" + page +
+                ", results=" + results +
+                ", total_pages=" + total_pages +
+                ", total_results=" + total_results +
+                '}';
+    }
+}
