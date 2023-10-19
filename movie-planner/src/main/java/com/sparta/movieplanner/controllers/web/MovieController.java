23fa7f474cf1321fb962cc1a7d73fa14d92e06cd
Jade Sale <jadesale1991@googlemail.com>
2023-10-19 18:00:18 +0100

    first working version of basic search bar functionality, returns title, description, rating and image

--- movie-planner/src/main/java/com/sparta/movieplanner/controllers/web/MovieController.java
+++ movie-planner/src/main/java/com/sparta/movieplanner/controllers/web/MovieController.java
@@ -1,28 +1,53 @@
 package com.sparta.movieplanner.controllers.web;
 
+import com.sparta.movieplanner.dto.MovieDTO;
 import com.sparta.movieplanner.services.MovieService;
+import com.sparta.movieplanner.tmdb.MovieShort;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class MovieController {
-
     Logger log = LoggerFactory.getLogger(MovieController.class);
-
+    private final String movieHtmlPagePath = "movie/searchMovie";
     @Autowired
     MovieService movieService;
 
-    // TODO: add or change the above to receive the request for a movie given a query
     @GetMapping("movies")
-    public String homepage() {
-        String movies = "movie/searchMovie";
-        log.info("loading homepage: " + movies + ".html");
+    public String movies(Model model) {
+        log.info("loading movies page: " + movieHtmlPagePath + ".html");
 
-        return movies;
+
+
+        return movieHtmlPagePath;
     }
+
+    // TODO: add or change the above to receive the request for a movie given a query
+
+    @PostMapping("movies/search")
+    public String searchMovies(@ModelAttribute("movieTitle") String title, Model model) {
+        log.info("movie search bar active");
+        log.info("move title from search bar: " + title);
+
+        List<MovieShort> moviesList = movieService.findMoviesByTitle(title);
+
+        if (!moviesList.isEmpty()) {
+            model.addAttribute("results_populated", true);
+            model.addAttribute("moviesList", moviesList);
+        } else {
+            model.addAttribute("results_populated", false);
+        }
+
+        return movieHtmlPagePath;
+    }
+
+
 }
