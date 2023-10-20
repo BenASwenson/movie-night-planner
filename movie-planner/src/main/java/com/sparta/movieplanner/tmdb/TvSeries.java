e6161fcded12ee8d0730c9aa37befcf2113ebb38
Elza Contiero <contieroelza@gmail.com>
2023-10-20 22:03:09 +0100

    MNP-47 Refactor Language class out of TvSeries so that it can be used elsewhere.

--- movie-planner/src/main/java/com/sparta/movieplanner/tmdb/TvSeries.java
+++ movie-planner/src/main/java/com/sparta/movieplanner/tmdb/TvSeries.java
@@ -388,44 +388,6 @@ public class TvSeries {
     }
     private ArrayList<Season> seasons;
 
-    public static class Language {
-        private String english_name;
-        private String iso_639_1;
-        private String name;
-
-        public String getEnglish_name() {
-            return english_name;
-        }
-
-        public void setEnglish_name(String english_name) {
-            this.english_name = english_name;
-        }
-
-        public String getIso_639_1() {
-            return iso_639_1;
-        }
-
-        public void setIso_639_1(String iso_639_1) {
-            this.iso_639_1 = iso_639_1;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public String toString() {
-            return "Language{" +
-                    "english_name='" + english_name + '\'' +
-                    ", iso_639_1='" + iso_639_1 + '\'' +
-                    '}';
-        }
-    }
-
     private ArrayList<Language> spoken_languages;
 
     private String status;
