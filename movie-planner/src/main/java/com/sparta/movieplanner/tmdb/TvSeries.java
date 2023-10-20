0566c32b192bd81f6db892f5800fab6b2e91b065
Elza Contiero <contieroelza@gmail.com>
2023-10-20 22:00:08 +0100

    MNP-47 Refactor Company class out of TvSeries so it can be used elsewhere as well.

--- movie-planner/src/main/java/com/sparta/movieplanner/tmdb/TvSeries.java
+++ movie-planner/src/main/java/com/sparta/movieplanner/tmdb/TvSeries.java
@@ -271,52 +271,6 @@ public class TvSeries {
     private double popularity;
     private String poster_path;
 
-    public static class Company {
-        private int id;
-        private String logo_path;
-        private String name;
-        private String origin_country;
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getLogo_path() {
-            return logo_path;
-        }
-
-        public void setLogo_path(String logo_path) {
-            this.logo_path = logo_path;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getOrigin_country() {
-            return origin_country;
-        }
-
-        public void setOrigin_country(String origin_country) {
-            this.origin_country = origin_country;
-        }
-
-        @Override
-        public String toString() {
-            return "Company{" +
-                    "id=" + id +
-                    ", name='" + name + '\'' +
-                    '}';
-        }
-    }
     private ArrayList<Company> production_companies;
 
     public static class Country {
