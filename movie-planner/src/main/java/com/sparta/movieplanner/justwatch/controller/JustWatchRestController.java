640ff942e08eb9eae679a6ef1cf3a106e52feb81
brhanuh <hanibalbrhanu15@hotmail.com>
2023-10-23 13:19:13 +0100

    Exception handling and code cleaning

--- movie-planner/src/main/java/com/sparta/movieplanner/justwatch/controller/JustWatchRestController.java
+++ movie-planner/src/main/java/com/sparta/movieplanner/justwatch/controller/JustWatchRestController.java
@@ -27,7 +27,7 @@ public class JustWatchRestController {
     @Autowired
     private ProviderService providerService;
 
-    @GetMapping("movie")
+    @GetMapping("movies")
     public Movie getMovie(@RequestParam String title, @RequestParam int year) throws IOException, InterruptedException {
         System.out.println(title);
         System.out.println(year);
@@ -35,7 +35,7 @@ public class JustWatchRestController {
         return movieService.findMovieByTitleAndReleaseYear(title, year);
     }
 
-    @GetMapping("movie/providers")
+    @GetMapping("movies/providers")
     public List<ProviderDTO> getMovieProvidersByTMDBId(@RequestParam int id) throws IOException, InterruptedException {
         return movieService.findAllProvidersForAMovieByTMDBId(id);
     }
