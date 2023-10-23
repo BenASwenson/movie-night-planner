bf57c56fb4ca4e05cdc7728a4f0c42496f65bd25
brhanuh <hanibalbrhanu15@hotmail.com>
2023-10-23 14:19:10 +0100

    fixed bean error

--- movie-planner/src/main/java/com/sparta/movieplanner/justwatch/controller/JustWatchRestController.java
+++ movie-planner/src/main/java/com/sparta/movieplanner/justwatch/controller/JustWatchRestController.java
@@ -9,6 +9,7 @@ import com.sparta.movieplanner.justwatch.service.ShowService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
+@RequestMapping("/justwatch")
 public class JustWatchRestController {
 
     @Autowired
