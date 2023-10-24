14d478f63a79dc81057f3718dc14b21fb409f42a
Jade Sale <jadesale1991@googlemail.com>
2023-10-24 19:26:41 +0100

    #MNP-17/#MNP-71 create database entities

---
+++ movie-planner/src/main/java/com/sparta/movieplanner/entities/Watchlist.java
@@ -0,0 +1,52 @@
+package com.sparta.movieplanner.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "watchlist")
+public class Watchlist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "movie_title", nullable = false)
+    private String movieTitle;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public void setMovieTitle(String movieTitle) {
+        this.movieTitle = movieTitle;
+    }
+
+    @Override
+    public String toString() {
+        return "Watchlist{" +
+                "id=" + id +
+                ", user=" + user +
+                ", movieTitle='" + movieTitle + '\'' +
+                '}';
+    }
+}
