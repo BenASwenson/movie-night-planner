36c3585dd2ec9d10efa852c580bcf30667a7602f
Jade Sale <jadesale1991@googlemail.com>
2023-10-25 17:14:53 +0100

    #MNP-17/#MNP-72 - created a method in watchlist service to handle logic for adding a users watchlist entry to the database

--- movie-planner/src/main/java/com/sparta/movieplanner/services/WatchlistService.java
+++ movie-planner/src/main/java/com/sparta/movieplanner/services/WatchlistService.java
@@ -1,19 +1,41 @@
 package com.sparta.movieplanner.services;
 
+import com.sparta.movieplanner.entities.Type;
+import com.sparta.movieplanner.entities.Watchlist;
+import com.sparta.movieplanner.repositories.UserRepository;
+import com.sparta.movieplanner.repositories.WatchlistRepository;
 import com.sparta.movieplanner.tmdb.MediaShort;
+import com.sparta.movieplanner.tmdb.MovieDetail;
 import com.sparta.movieplanner.tmdb.Tmdb;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class WatchlistService {
     Logger log = LoggerFactory.getLogger(getClass());
+    @Autowired
     private Tmdb tmdb;
-
+    @Autowired
     private MovieService movieService;
+    @Autowired
+    private WatchlistRepository watchlistRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-    //TODO: might need to use movie service to get movies
+    public Watchlist createMovieWatchlistEntry(String movieTitle, int movieId, String username ) {
+        Watchlist newWatchlistEntry = new Watchlist();
+        newWatchlistEntry.setId(0L);
+        newWatchlistEntry.setUser(userRepository.findByUsername(username).get());
+        newWatchlistEntry.setTitleId(movieId);
+        newWatchlistEntry.setTitle(movieTitle);
+        newWatchlistEntry.setType(Type.FILM);
+        return newWatchlistEntry;
+    }
+    
+
 }
